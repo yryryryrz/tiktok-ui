a88a335a061ad496fff8916d4ed67ae8e2b77a19
yryryryrz <poison.13h12@gmail.com>
2023-06-27 03:23:08 +0700

    chia layout, router 27/6

---
+++ src/components/Layout/HeaderOnly/index.js
@@ -0,0 +1,13 @@
+import Header from "../componentsHeader/Header";
+function defaultLayout({ children }) {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <div className="content">{children}</div>
+            </div>
+        </div>
+    );
+}
+
+export default defaultLayout;
---
+++ src/pages/Profile/index.js
@@ -0,0 +1,4 @@
+function Profile() {
+    return <h2>Profile page</h2>;
+}
+export default Profile;
